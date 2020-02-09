d2fd9252c890585fd09d66e91ed59ed398099ccd
sptkl <caobaiyue@gmail.com>
2020-02-08 19:19:23 -0500

    switch to socrata api

--- app.py
+++ app.py
@@ -5,16 +5,21 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
-import plotly.express as px
+from datetime import datetime as dt
+from datetime import timedelta
+from datetime import date
+from dotenv import load_dotenv
 import requests
 import json
-import plotly.graph_objects as go
+import os
 
+load_dotenv()
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 r = json.loads(requests.get('https://cwhong.carto.com/api/v2/sql?q=select distinct agency from public.table_311_1').content)
 agency_options = [{'label':i['agency'], 'value':i['agency']} for i in r['rows']]
+headers = {'X-App-Token':os.environ['API_TOKEN']}
+url='https://data.cityofnewyork.us/resource/erm2-nwe9.json'
 
 app.layout = html.Div([
     dcc.Dropdown(
@@ -22,30 +27,47 @@ app.layout = html.Div([
         options=agency_options,
         value='TLC'
     ), 
+    dcc.DatePickerRange(
+        id='date-picker',
+        min_date_allowed=date.today()-timedelta(days=7),
+        max_date_allowed=date.today(),
+        initial_visible_month=date.today(),
+        start_date=date.today()-timedelta(days=7),
+        end_date=date.today()
+    ),
     html.Div(id='map-area')
 ])
 
-
 @app.callback(
     dash.dependencies.Output('map-area', 'children'),
-    [dash.dependencies.Input('agency-dropdown', 'value')])
-def update_output(agency):
-    r = json.loads(requests.get(f"https://cwhong.carto.com/api/v2/sql?q=select * from public.table_311_1 where agency = '{agency}'").content)['rows']
+    [dash.dependencies.Input('agency-dropdown', 'value'),
+    dash.dependencies.Input('date-picker', 'start_date'),
+    dash.dependencies.Input('date-picker', 'end_date')])
+def update_output(agency, start_date, end_date):
+    params = {
+            '$select':'longitude,latitude', 
+            '$where':f"agency='{agency}' AND longitude IS NOT NULL AND latitude IS NOT NULL AND created_date>'{start_date}' AND created_date<'{end_date}'",
+            '$limit':50000
+            }
+
+    r = json.loads(
+            requests.get(f"{url}", headers=headers, params=params).content)
     return html.Div([
     dcc.Graph(
         id='agency-plot',
         figure={
             'data': [{
-                'type': 'scattermapbox',
-                'lon': [i['longitude'] for i in r],
-                'lat': [i['latitude'] for i in r],
+                'type': 'densitymapbox',
+                'lon': [float(i['longitude']) for i in r],
+                'lat': [float(i['latitude']) for i in r],
+                'radius':10
             }],
             'layout': {
                 'height': 700,
                 'margin': {"r":0,"t":0,"l":0,"b":0},
                 'mapbox': {
                     'center': {"lat": 40.722469759433, "lon": -73.9871350560601},
-                    'zoom' : 14,
+                    'zoom' : 11,
                     'style':"carto-positron"
                 }    
             }
@@ -53,7 +75,7 @@ def update_output(agency):
     ])
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, port=5000)
 
 
 
